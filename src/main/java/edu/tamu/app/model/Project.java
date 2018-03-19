ce6fa3be6d5f8d6a0e35ab0048a338f67eeb7386
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2018-03-19 13:23:22 -0500

    Created JUnit tests for project model and controller

--- src/main/java/edu/tamu/app/model/Project.java
+++ src/main/java/edu/tamu/app/model/Project.java
@@ -1,17 +1,24 @@
 package edu.tamu.app.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 
 import edu.tamu.app.model.validation.ProjectValidator;
 import edu.tamu.weaver.validation.model.ValidatingBaseEntity;
 
+@Entity
 public class Project extends ValidatingBaseEntity  {
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String name;
     
     public Project() {
         setModelValidator(new ProjectValidator());
+    }
+    
+    public Project(String name) {
+        this();
+        setName(name);
     }
 
     public String getName() {
---
+++ src/test/java/edu/tamu/app/model/ProjectTest.java
@@ -0,0 +1,65 @@
+package edu.tamu.app.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import edu.tamu.app.ProjectApplication;
+import edu.tamu.app.model.repo.ProjectRepo;
+
+@WebAppConfiguration
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { ProjectApplication.class })
+public class ProjectTest {
+    
+    protected static final String TEST_PROJECT_NAME = "Test Project Name";
+    protected static final String TEST_ALTERNATE_PROJECT_NAME = "Alternate Project Name";
+    
+    @Autowired
+    ProjectRepo projectRepo;
+    
+    @Test
+    public void testCreate() {
+        long initalCount = projectRepo.count();
+        projectRepo.create(new Project(TEST_PROJECT_NAME));
+        assertEquals("The number of Projects did not increase by one", initalCount + 1, projectRepo.count());
+    }
+    
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testNameNotNull() {
+        projectRepo.create(new Project(null));
+    }
+    
+    @Test
+    public void testUpdateName() {
+        Project project = projectRepo.create(new Project(TEST_PROJECT_NAME));
+        project.setName(TEST_ALTERNATE_PROJECT_NAME);
+        projectRepo.save(project);
+        project = projectRepo.findOne(project.getId());
+        assertEquals("Project name was not changed", TEST_ALTERNATE_PROJECT_NAME, project.getName());
+    }
+    
+    @Test
+    public void testDelete() {
+        long initialCount = projectRepo.count();
+        Project project = projectRepo.create(new Project(TEST_ALTERNATE_PROJECT_NAME));
+        assertEquals("Project not created", initialCount + 1, projectRepo.count());
+        projectRepo.delete(project);
+        assertEquals("Project was not deleted", initialCount, projectRepo.count());
+    }
+    
+    @After
+    public void cleanUp() {
+        projectRepo.deleteAll();
+    }
+
+}
