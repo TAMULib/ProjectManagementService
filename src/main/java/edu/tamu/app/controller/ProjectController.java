d551e3446ce663d8e393f048fa792107bf7baf0c
William Welling <wwelling@library.tamu.edu>
2018-03-22 10:36:33 -0500

    updated endpoints for projects

--- src/main/java/edu/tamu/app/controller/ProjectController.java
+++ src/main/java/edu/tamu/app/controller/ProjectController.java
@@ -9,33 +9,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.tamu.app.model.Project;
-import edu.tamu.app.model.User;
 import edu.tamu.app.model.repo.ProjectRepo;
-import edu.tamu.weaver.auth.annotation.WeaverUser;
 import edu.tamu.weaver.response.ApiResponse;
 import edu.tamu.weaver.validation.aspect.annotation.WeaverValidatedModel;
 import edu.tamu.weaver.validation.aspect.annotation.WeaverValidation;
 
 @RestController
-@RequestMapping("/project")
+@RequestMapping("/projects")
 public class ProjectController {
 
     @Autowired
     private ProjectRepo projectRepo;
-    
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-    
+
     @RequestMapping(method = RequestMethod.GET)
     @PreAuthorize("hasRole('ANONYMOUS')")
-    public ApiResponse getAll(@WeaverUser User user) {
+    public ApiResponse getAll() {
         return new ApiResponse(SUCCESS, projectRepo.findAll());
     }
-    
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ANONYMOUS')")
+    public ApiResponse getOne(@PathVariable Long id) {
+        return new ApiResponse(SUCCESS, projectRepo.findOne(id));
+    }
+
     @RequestMapping(method = RequestMethod.POST)
     @PreAuthorize("hasRole('USER')")
     @WeaverValidation(business = { @WeaverValidation.Business(value = CREATE) })
@@ -43,7 +48,7 @@ public class ProjectController {
         logger.info("Creating Project: " + project.getName());
         return new ApiResponse(SUCCESS, projectRepo.create(project));
     }
-    
+
     @RequestMapping(method = RequestMethod.PUT)
     @PreAuthorize("hasRole('USER')")
     @WeaverValidation(business = { @WeaverValidation.Business(value = UPDATE) })
@@ -51,7 +56,7 @@ public class ProjectController {
         logger.info("Updating Project: " + project.getName());
         return new ApiResponse(SUCCESS, projectRepo.update(project));
     }
-    
+
     @RequestMapping(method = RequestMethod.DELETE)
     @PreAuthorize("hasRole('USER')")
     @WeaverValidation(business = { @WeaverValidation.Business(value = DELETE) })
