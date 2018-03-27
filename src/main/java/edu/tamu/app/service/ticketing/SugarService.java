3094caf50dca8ff8e5dc732365ce841fe4c971d2
William Welling <wwelling@library.tamu.edu>
2018-03-27 10:48:56 -0500

    added submitter credentials to sugar issue

--- src/main/java/edu/tamu/app/service/ticketing/SugarService.java
+++ src/main/java/edu/tamu/app/service/ticketing/SugarService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import edu.tamu.app.model.request.TicketRequest;
+import edu.tamu.weaver.auth.model.Credentials;
 import edu.tamu.weaver.email.service.EmailSender;
 
 @Service
@@ -23,12 +24,22 @@ public class SugarService implements TicketManagementSoftwareBean {
     public String submit(TicketRequest request) {
         String results = "Unable to submit ticket to sugar at this time!";
         try {
-            emailService.sendEmail(reportingAddress, request.getService() + " Issue: " + request.getTitle(), request.getDescription());
-            results = "Successfully submitted issue " + request.getService() + "!";
+            emailService.sendEmail(reportingAddress, getSubject(request), getBody(request));
+            results = "Successfully submitted issue for " + request.getService() + "!";
         } catch (MessagingException e) {
             e.printStackTrace();
         }
         return results;
+    }
+
+    private String getSubject(TicketRequest request) {
+        return request.getService() + " Issue: " + request.getTitle();
+    }
+
+    private String getBody(TicketRequest request) {
+        Credentials submitter = request.getCredentials();
+        return request.getDescription() + "<br/><br/>Submitted by: " + submitter.getLastName() + ", " + submitter.getFirstName() + " (" + submitter.getEmail() + ")";
+
     }
 
 }
