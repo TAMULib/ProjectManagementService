e37ad353fe5be7718e5fb8b68c2db5376db242f6
William Welling <wwelling@library.tamu.edu>
2018-06-26 16:35:25 -0500

    update user model

--- src/main/java/edu/tamu/app/controller/UserController.java
+++ src/main/java/edu/tamu/app/controller/UserController.java
@@ -1,10 +1,8 @@
 package edu.tamu.app.controller;
 
-import static edu.tamu.weaver.response.ApiStatus.ERROR;
 import static edu.tamu.weaver.response.ApiStatus.SUCCESS;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.tamu.app.model.User;
 import edu.tamu.app.model.repo.UserRepo;
 import edu.tamu.weaver.auth.annotation.WeaverCredentials;
-import edu.tamu.weaver.auth.annotation.WeaverUser;
 import edu.tamu.weaver.auth.model.Credentials;
 import edu.tamu.weaver.response.ApiResponse;
 
@@ -27,11 +24,8 @@ public class UserController {
     @Autowired
     private UserRepo userRepo;
 
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
-
     /**
-     * Websocket endpoint to request credentials.
+     * Get credentials.
      * 
      * @param credentials
      * @WeaverCredentials Credentials
@@ -40,30 +34,11 @@ public class UserController {
     @RequestMapping("/credentials")
     @PreAuthorize("hasRole('ANONYMOUS')")
     public ApiResponse credentials(@WeaverCredentials Credentials credentials) {
-        if (credentials == null) {
-            return new ApiResponse(ERROR, "Unable to retrieve credentials!");
-        }
         return new ApiResponse(SUCCESS, credentials);
     }
 
     /**
-     * Endpoint to get user.
-     * 
-     * @param user
-     * @ApiModel AppUser
-     * @return ApiResponse
-     */
-    @RequestMapping("/user")
-    @PreAuthorize("hasRole('USER')")
-    public ApiResponse getUser(@WeaverUser User user) {
-        if (user == null) {
-            return new ApiResponse(ERROR, "Unable to retrieve user!");
-        }
-        return new ApiResponse(SUCCESS, user);
-    }
-
-    /**
-     * Endpoint to get all users.
+     * Get all users.
      * 
      * @return ApiResponse
      */
@@ -74,25 +49,24 @@ public class UserController {
     }
 
     /**
-     * Endpoint to update user.
+     * Update user.
      * 
      * @param user
-     * @ApiModel AppUser
+     * @RequestBody User
      * @return ApiResponse
      */
     @RequestMapping("/update")
     @PreAuthorize("hasRole('MANAGER')")
     public ApiResponse updateUser(@RequestBody User user) {
-        user = userRepo.save(user);
-        simpMessagingTemplate.convertAndSend("/channel/users", new ApiResponse(SUCCESS, userRepo.findAll()));
+        user = userRepo.update(user);
         return new ApiResponse(SUCCESS, user);
     }
 
     /**
-     * Endpoint to delete user.
+     * Delete user.
      * 
      * @param user
-     * @ApiModel AppUser
+     * @RequestBody User
      * @return ApiResponse
      */
     @RequestMapping("/delete")
