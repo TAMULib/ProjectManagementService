e69139fed9ba88456a7ff4c62035b52ad81eeb88
William Welling <wwelling@library.tamu.edu>
2018-08-13 08:48:27 -0500

    some additional test coverage

--- src/main/java/edu/tamu/app/rest/BasicAuthRestTemplate.java
+++ src/main/java/edu/tamu/app/rest/BasicAuthRestTemplate.java
@@ -1,33 +1,35 @@
 package edu.tamu.app.rest;
 
 import java.util.Collections;
-import java.util.List;
 
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.InterceptingClientHttpRequestFactory;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 public class BasicAuthRestTemplate extends RestTemplate {
 
-    private String username;
+    private final String username;
 
-    private String password;
+    private final String password;
 
     public BasicAuthRestTemplate(String username, String password) {
         super();
         this.username = username;
         this.password = password;
-        addAuthentication();
-    }
 
-    private void addAuthentication() {
         if (StringUtils.isEmpty(username)) {
             throw new RuntimeException("Username is mandatory for Basic Auth");
         }
 
-        List<ClientHttpRequestInterceptor> interceptors = Collections.singletonList(new BasicAuthInterceptor(username, password));
-        setRequestFactory(new InterceptingClientHttpRequestFactory(getRequestFactory(), interceptors));
+        setRequestFactory(new InterceptingClientHttpRequestFactory(getRequestFactory(), Collections.singletonList(new BasicAuthInterceptor(username, password))));
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
 }
--- src/test/java/edu/tamu/app/controller/ProjectControllerUnitTest.java
+++ src/test/java/edu/tamu/app/controller/ProjectControllerUnitTest.java
@@ -5,9 +5,9 @@ import static edu.tamu.weaver.response.ApiStatus.SUCCESS;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,15 +19,16 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.versionone.apiclient.exceptions.APIException;
-import com.versionone.apiclient.exceptions.ConnectionException;
-import com.versionone.apiclient.exceptions.OidException;
-
+import edu.tamu.app.cache.model.RemoteProject;
+import edu.tamu.app.cache.service.ActiveSprintsScheduledCacheService;
 import edu.tamu.app.cache.service.ProjectScheduledCache;
+import edu.tamu.app.cache.service.ProjectsStatsScheduledCacheService;
+import edu.tamu.app.cache.service.RemoteProjectsScheduledCacheService;
 import edu.tamu.app.model.Project;
 import edu.tamu.app.model.RemoteProjectManager;
 import edu.tamu.app.model.ServiceType;
@@ -57,7 +58,7 @@ public class ProjectControllerUnitTest {
     private static final String NO_PROJECT_ERROR_MESSAGE = "Project with id null not found!";
     private static final String INVALID_RPM_ID_ERROR_MESSAGE = "Error fetching remote projects from Test Remote Project Manager!";
     private static final String MISSING_RPM_ERROR_MESSAGE = "Remote Project Manager with id null not found!";
-    private static final String INVALID_RPM_ID_ERROR_MESSAGE_FIND_BY_ID = "Error fetching remote project with scope id null from Test Remote Project Manager!";
+    private static final String INVALID_RPM_ID_ERROR_MESSAGE_FIND_BY_ID = "Error fetching remote project with scope id " + TEST_PROJECT1_SCOPE + " from Test Remote Project Manager!";
 
     private static final RemoteProjectManager TEST_PROJECT1_REMOTE_PROJECT_MANAGER = new RemoteProjectManager("Test Remote Project Manager", ServiceType.VERSION_ONE, new HashMap<String, String>());
 
@@ -67,9 +68,14 @@ public class ProjectControllerUnitTest {
     private static Project TEST_PROJECT_WIHTOUT_RPM = new Project(TEST_PROJECT_WITHOUT_RPM_NAME);
 
     private static TicketRequest TEST_TICKET_REQUEST = new TicketRequest();
+
+    private static FeatureRequest TEST_FEATURE_REQUEST = new FeatureRequest(TEST_FEATURE_REQUEST_TITLE, TEST_FEATURE_REQUEST_DESCRIPTION, TEST_PROJECT1.getId(), TEST_PROJECT1_SCOPE);
+
+    // NOTE: this is not really an invalid feature request
     private static FeatureRequest TEST_INVALID_FEATURE_REQUEST = new FeatureRequest(TEST_FEATURE_REQUEST_TITLE, TEST_FEATURE_REQUEST_DESCRIPTION, TEST_PROJECT1.getId(), TEST_PROJECT1_SCOPE);
     private static FeatureRequest TEST_FEATURE_REQUEST_WIHTOUT_VMS = new FeatureRequest(TEST_FEATURE_REQUEST_TITLE, TEST_FEATURE_REQUEST_DESCRIPTION, TEST_PROJECT_WIHTOUT_RPM.getId(), TEST_PROJECT2_SCOPE);
     private static FeatureRequest TEST_FEATURE_REQUEST_WITHOUT_PROJECT = new FeatureRequest();
+
     private static List<Project> mockProjectList = new ArrayList<Project>(Arrays.asList(new Project[] { TEST_PROJECT1, TEST_PROJECT2 }));
 
     private static ApiResponse apiResponse;
@@ -87,16 +93,18 @@ public class ProjectControllerUnitTest {
     private ManagementBeanRegistry managementBeanRegistry;
 
     @Mock
-    private RemoteProjectManagerBean managementBean;
+    private RemoteProjectManagerBean remoteProjectManagementBean;
 
-    @Spy
-    private List<ProjectScheduledCache<?, ?>> projectSceduledCaches = new ArrayList<ProjectScheduledCache<?, ?>>();
+    @Mock
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Mock
+    private RemoteProjectsScheduledCacheService remoteProjectsScheduledCacheService;
 
     @InjectMocks
     private ProjectController projectController;
 
     @Before
-    @SuppressWarnings("unchecked")
     public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
         when(projectRepo.findAll()).thenReturn(mockProjectList);
@@ -106,7 +114,31 @@ public class ProjectControllerUnitTest {
         when(remoteProjectManagerRepo.findOne(any(Long.class))).thenReturn(TEST_PROJECT1_REMOTE_PROJECT_MANAGER);
         doNothing().when(projectRepo).delete(any(Project.class));
         when(sugarService.submit(any(TicketRequest.class))).thenReturn("Successfully submitted issue for test service!");
-        when(managementBean.push(TEST_INVALID_FEATURE_REQUEST)).thenThrow(Exception.class);
+
+        TEST_PROJECT1.setId(1L);
+        TEST_PROJECT2.setId(2L);
+
+        ProjectsStatsScheduledCacheService projectsStatsScheduledCacheService = mock(ProjectsStatsScheduledCacheService.class);
+
+        ActiveSprintsScheduledCacheService activeSprintsScheduledCacheService = mock(ActiveSprintsScheduledCacheService.class);
+
+        doNothing().when(projectsStatsScheduledCacheService).addProject(any(Project.class));
+        doNothing().when(projectsStatsScheduledCacheService).updateProject(any(Project.class));
+        doNothing().when(projectsStatsScheduledCacheService).removeProject(any(Project.class));
+
+        doNothing().when(activeSprintsScheduledCacheService).addProject(any(Project.class));
+        doNothing().when(activeSprintsScheduledCacheService).updateProject(any(Project.class));
+        doNothing().when(activeSprintsScheduledCacheService).removeProject(any(Project.class));
+
+        List<ProjectScheduledCache<?, ?>> projectSceduledCaches = new ArrayList<ProjectScheduledCache<?, ?>>() {
+            private static final long serialVersionUID = 621069988291823739L;
+            {
+                add(projectsStatsScheduledCacheService);
+                add(activeSprintsScheduledCacheService);
+            }
+        };
+
+        ReflectionTestUtils.setField(projectController, "projectSceduledCaches", projectSceduledCaches);
     }
 
     @Test
@@ -154,14 +186,18 @@ public class ProjectControllerUnitTest {
     }
 
     @Test
-    public void testPushRequest() throws JsonProcessingException, IOException {
-
+    public void testPushRequest() throws Exception {
+        when(remoteProjectManagementBean.push(TEST_FEATURE_REQUEST)).thenReturn(TEST_FEATURE_REQUEST);
+        when(managementBeanRegistry.getService(any(String.class))).thenReturn(remoteProjectManagementBean);
+        when(projectRepo.findOne(any(Long.class))).thenReturn(TEST_PROJECT1);
+        apiResponse = projectController.pushRequest(new MockHttpServletRequest(), TEST_FEATURE_REQUEST);
+        assertEquals("Project controller did not push request", SUCCESS, apiResponse.getMeta().getStatus());
     }
 
     @Test
     public void testPushRequestToInvalidRemoteProjectManager() {
         when(projectRepo.findOne(any(Long.class))).thenReturn(TEST_PROJECT1);
-        apiResponse = projectController.pushRequest(null, TEST_INVALID_FEATURE_REQUEST);
+        apiResponse = projectController.pushRequest(new MockHttpServletRequest(), TEST_INVALID_FEATURE_REQUEST);
         assertEquals("Invalid push did not throw an exception", ERROR, apiResponse.getMeta().getStatus());
         assertEquals("Push without Remote Project Manager did not result in the expected error", PUSH_ERROR_MESSAGE, apiResponse.getMeta().getMessage());
     }
@@ -169,21 +205,24 @@ public class ProjectControllerUnitTest {
     @Test
     public void testPushRequestWithoutRemoteProjectManager() {
         when(projectRepo.findOne(any(Long.class))).thenReturn(TEST_PROJECT_WIHTOUT_RPM);
-        apiResponse = projectController.pushRequest(null, TEST_FEATURE_REQUEST_WIHTOUT_VMS);
+        apiResponse = projectController.pushRequest(new MockHttpServletRequest(), TEST_FEATURE_REQUEST_WIHTOUT_VMS);
         assertEquals("Push without Remote Project Manager did not result in an error", ERROR, apiResponse.getMeta().getStatus());
         assertEquals("Push without Remote Project Manager did not result in the expected error", NO_RPM_ERROR_MESSAGE, apiResponse.getMeta().getMessage());
     }
 
     @Test
     public void testPushRequestWithoutProject() {
-        apiResponse = projectController.pushRequest(null, TEST_FEATURE_REQUEST_WITHOUT_PROJECT);
+        apiResponse = projectController.pushRequest(new MockHttpServletRequest(), TEST_FEATURE_REQUEST_WITHOUT_PROJECT);
         assertEquals("Push without Project did not result in an error", ERROR, apiResponse.getMeta().getStatus());
         assertEquals("Push without Project did not result in the expected error", NO_PROJECT_ERROR_MESSAGE, apiResponse.getMeta().getMessage());
     }
 
     @Test
-    public void testGetAllRemoteProjects() throws JsonProcessingException, IOException, ConnectionException, APIException, OidException {
-
+    public void testGetAllRemoteProjects() throws Exception {
+        when(remoteProjectManagementBean.push(TEST_FEATURE_REQUEST)).thenReturn(TEST_FEATURE_REQUEST);
+        when(managementBeanRegistry.getService(any(String.class))).thenReturn(remoteProjectManagementBean);
+        apiResponse = projectController.getAllRemoteProjects(TEST_PROJECT1_REMOTE_PROJECT_MANAGER.getId());
+        assertEquals("Project controller unable to get all remote projects", SUCCESS, apiResponse.getMeta().getStatus());
     }
 
     @Test
@@ -202,13 +241,16 @@ public class ProjectControllerUnitTest {
     }
 
     @Test
-    public void testGetRemoteProjectByScopeId() throws JsonProcessingException, IOException, ConnectionException, APIException, OidException {
-
+    public void testGetRemoteProjectByScopeId() throws Exception {
+        when(remoteProjectManagementBean.getRemoteProjectByScopeId(TEST_PROJECT1_SCOPE)).thenReturn(new RemoteProject());
+        when(managementBeanRegistry.getService(any(String.class))).thenReturn(remoteProjectManagementBean);
+        apiResponse = projectController.getRemoteProjectByScopeId(TEST_PROJECT1_REMOTE_PROJECT_MANAGER.getId(), TEST_PROJECT1_SCOPE);
+        assertEquals("Project controller unable to get remote project by scope id", SUCCESS, apiResponse.getMeta().getStatus());
     }
 
     @Test
     public void testGetRemoteProjectByScopeIdWithInvalidRemoteProjectManager() {
-        apiResponse = projectController.getRemoteProjectByScopeId(TEST_PROJECT1_REMOTE_PROJECT_MANAGER.getId(), null);
+        apiResponse = projectController.getRemoteProjectByScopeId(TEST_PROJECT1_REMOTE_PROJECT_MANAGER.getId(), TEST_PROJECT1_SCOPE);
         assertEquals("Request with invalid Remote Project Manager id did not result in an error", ERROR, apiResponse.getMeta().getStatus());
         assertEquals("Invalid Remote Project Manager id did not result in the expected error message", INVALID_RPM_ID_ERROR_MESSAGE_FIND_BY_ID, apiResponse.getMeta().getMessage());
     }
@@ -216,7 +258,7 @@ public class ProjectControllerUnitTest {
     @Test
     public void testGetRemoteProjectByScopeIdWithMissingRemoteProjectManager() {
         when(remoteProjectManagerRepo.findOne(any(Long.class))).thenReturn(null);
-        apiResponse = projectController.getRemoteProjectByScopeId(TEST_PROJECT1_REMOTE_PROJECT_MANAGER.getId(), null);
+        apiResponse = projectController.getRemoteProjectByScopeId(TEST_PROJECT1_REMOTE_PROJECT_MANAGER.getId(), TEST_PROJECT1_SCOPE);
         assertEquals("Request with no Remote Project Manager did not result in an error", ERROR, apiResponse.getMeta().getStatus());
         assertEquals("Missing Remote Project Manager did not result in the expected error message", MISSING_RPM_ERROR_MESSAGE, apiResponse.getMeta().getMessage());
     }
