7e264ea720562668dabf4f5612d49bac20960d1c
William Welling <wwelling@library.tamu.edu>
2018-08-14 10:31:48 -0500

    by default map Story to Feature

--- src/main/java/edu/tamu/app/ProjectInitialization.java
+++ src/main/java/edu/tamu/app/ProjectInitialization.java
@@ -1,9 +1,14 @@
 package edu.tamu.app;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import edu.tamu.app.model.CardType;
+import edu.tamu.app.model.repo.CardTypeRepo;
 import edu.tamu.app.model.repo.RemoteProjectManagerRepo;
 import edu.tamu.app.service.registry.ManagementBeanRegistry;
 
@@ -16,11 +21,17 @@ public class ProjectInitialization implements CommandLineRunner {
     @Autowired
     private RemoteProjectManagerRepo remoteProjectManagerRepo;
 
+    @Autowired
+    private CardTypeRepo cardTypeRepo;
+
     @Override
     public void run(String... args) throws Exception {
         remoteProjectManagerRepo.findAll().forEach(versionManagementSoftware -> {
             managementBeanRegistry.register(versionManagementSoftware);
         });
+        if (cardTypeRepo.findByIdentifier("Feature") == null) {
+            cardTypeRepo.create(new CardType("Feature", new HashSet<String>(Arrays.asList(new String[] { "Story" }))));
+        }
     }
 
 }
