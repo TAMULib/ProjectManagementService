536b190bcd893339555f43f36c8a1ba490b3f7b8
William Welling <wwelling@library.tamu.edu>
2018-08-14 10:09:31 -0500

    trigger asynchronous remote project cache update on remote project manager create

--- src/main/java/edu/tamu/app/model/repo/impl/RemoteProjectManagerRepoImpl.java
+++ src/main/java/edu/tamu/app/model/repo/impl/RemoteProjectManagerRepoImpl.java
@@ -1,7 +1,11 @@
 package edu.tamu.app.model.repo.impl;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import edu.tamu.app.cache.service.RemoteProjectsScheduledCacheService;
 import edu.tamu.app.model.RemoteProjectManager;
 import edu.tamu.app.model.repo.RemoteProjectManagerRepo;
 import edu.tamu.app.model.repo.custom.RemoteProjectManagerRepoCustom;
@@ -10,13 +14,23 @@ import edu.tamu.weaver.data.model.repo.impl.AbstractWeaverRepoImpl;
 
 public class RemoteProjectManagerRepoImpl extends AbstractWeaverRepoImpl<RemoteProjectManager, RemoteProjectManagerRepo> implements RemoteProjectManagerRepoCustom {
 
+    private static final Logger logger = Logger.getLogger(RemoteProjectManagerRepoImpl.class);
+
     @Autowired
     private ManagementBeanRegistry managementBeanRegistry;
+
+    @Autowired
+    private RemoteProjectsScheduledCacheService remoteProjectsScheduledCacheService;
 
     @Override
     public RemoteProjectManager create(RemoteProjectManager remoteProjectManager) {
         remoteProjectManager = super.create(remoteProjectManager);
         managementBeanRegistry.register(remoteProjectManager);
+        CompletableFuture.runAsync(() -> {
+            remoteProjectsScheduledCacheService.schedule();
+        }).thenRun(() -> {
+            logger.info("Finished asynchronous cache update and boredcast of remote projects");
+        });
         return remoteProjectManager;
     }
 
