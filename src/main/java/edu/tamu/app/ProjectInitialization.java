5d063da4e647ff76b1b6270f8e4792cc961544c7
William Welling <wwelling@library.tamu.edu>
2018-08-14 10:47:59 -0500

    dont run project initialization for tests

--- src/main/java/edu/tamu/app/ProjectInitialization.java
+++ src/main/java/edu/tamu/app/ProjectInitialization.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import edu.tamu.app.model.CardType;
@@ -13,6 +14,7 @@ import edu.tamu.app.model.repo.RemoteProjectManagerRepo;
 import edu.tamu.app.service.registry.ManagementBeanRegistry;
 
 @Component
+@Profile("!test")
 public class ProjectInitialization implements CommandLineRunner {
 
     @Autowired
