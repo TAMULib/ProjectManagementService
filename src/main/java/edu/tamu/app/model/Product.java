d0bffdab4c5371773565942dc4d880205b77e80b
Kevin Day <kday@library.tamu.edu>
2020-05-11 08:56:21 -0500

    Add missing getters/setters and also add related tests

--- src/main/java/edu/tamu/app/model/Product.java
+++ src/main/java/edu/tamu/app/model/Product.java
@@ -67,12 +67,63 @@ public class Product extends ValidatingBaseEntity {
         this.remoteProducts = remoteProducts;
     }
 
+    public Product(String name, List<RemoteProductInfo> remoteProducts, String scopeId, String devUrl, String preUrl, String productionUrl, String wikiUrl) {
+        this();
+        this.name = name;
+        this.remoteProducts = remoteProducts;
+        this.scopeId = scopeId;
+        this.devUrl = devUrl;
+        this.preUrl = preUrl;
+        this.productionUrl = productionUrl;
+        this.wikiUrl = wikiUrl;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getScopeId() {
+        return scopeId;
+    }
+
+    public void setScopeId(String scopeId) {
+        this.scopeId = scopeId;
+    }
+
+    public String getDevUrl() {
+        return devUrl;
+    }
+
+    public void setDevUrl(String devUrl) {
+        this.devUrl = devUrl;
+    }
+
+    public String getPreUrl() {
+        return preUrl;
+    }
+
+    public void setPreUrl(String preUrl) {
+        this.preUrl = preUrl;
+    }
+
+    public String getProductionUrl() {
+        return productionUrl;
+    }
+
+    public void setProductionUrl(String productionUrl) {
+        this.productionUrl = productionUrl;
+    }
+
+    public String getWikiUrl() {
+        return wikiUrl;
+    }
+
+    public void setWikiUrl(String wikiUrl) {
+        this.wikiUrl = wikiUrl;
     }
 
     public List<RemoteProductInfo> getRemoteProducts() {
