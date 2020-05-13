2c8fe9e669b20059c632a593837986a92645bed0
Ryan Laddusaw <rladdusaw@gmail.com>
2020-05-13 12:04:13 -0500

    Fixed sprint update issue and validating urls

--- src/main/java/edu/tamu/app/model/validation/ProductValidator.java
+++ src/main/java/edu/tamu/app/model/validation/ProductValidator.java
@@ -9,5 +9,17 @@ public class ProductValidator extends BaseModelValidator {
     public ProductValidator() {
         String nameProperty = "name";
         this.addInputValidator(new InputValidator(InputValidationType.required, "A Product requires a name", nameProperty, true));
+
+        String devUrlProperty = "devUrl";
+        this.addInputValidator(new InputValidator(InputValidationType.pattern, "The development URL must be a valid URL", devUrlProperty, "^(https?|ftp|file|wss?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]+[-a-zA-Z0-9+&@#/%=~_|?]"));
+
+        String preUrlProperty = "preUrl";
+        this.addInputValidator(new InputValidator(InputValidationType.pattern, "The pre-production URL must be a valid URL", preUrlProperty, "^(https?|ftp|file|wss?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]+[-a-zA-Z0-9+&@#/%=~_|?]"));
+
+        String productionUrlProperty = "productionUrl";
+        this.addInputValidator(new InputValidator(InputValidationType.pattern, "The production URL must be a valid URL", productionUrlProperty, "^(https?|ftp|file|wss?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]+[-a-zA-Z0-9+&@#/%=~_|?]"));
+
+        String wikiUrlProperty = "wikiUrl";
+        this.addInputValidator(new InputValidator(InputValidationType.pattern, "The wiki URL must be a valid URL", wikiUrlProperty, "^(https?|ftp|file|wss?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]+[-a-zA-Z0-9+&@#/%=~_|?]"));
     }
 }
