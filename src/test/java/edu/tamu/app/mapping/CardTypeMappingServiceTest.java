e7b5c6dffdd87fda629becb6f5494bb1970fa45a
William Welling <8352733+wwelling@users.noreply.github.com>
2022-08-02 11:46:16 -0500

    fix card type mapping service test assert argument order

--- src/test/java/edu/tamu/app/mapping/CardTypeMappingServiceTest.java
+++ src/test/java/edu/tamu/app/mapping/CardTypeMappingServiceTest.java
@@ -8,17 +8,14 @@ import java.util.HashSet;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import edu.tamu.app.ProductApplication;
 import edu.tamu.app.model.CardType;
 import edu.tamu.app.model.repo.CardTypeRepo;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = { ProductApplication.class }, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CardTypeMappingServiceTest {
 
@@ -35,12 +32,12 @@ public class CardTypeMappingServiceTest {
 
     @Test
     public void testMap() {
-        assertEquals(cardTypeMappingService.map("Story"), "Feature");
+        assertEquals("Feature", cardTypeMappingService.map("Story"));
     }
 
     @Test
     public void testHandleUnmapped() {
-        assertEquals(cardTypeMappingService.handleUnmapped("Defect"), "Handled unmapped incorrectly!", "Defect");
+        assertEquals("Defect", cardTypeMappingService.handleUnmapped("Defect"), "Handled unmapped incorrectly!");
     }
 
     @AfterEach
