c194cfafe27ec18ea887a841e73bc17a4434e783
William Welling <8352733+wwelling@users.noreply.github.com>
2022-08-03 10:36:52 -0500

    try thenAnswer

--- src/test/java/edu/tamu/app/service/manager/GitHubProjectServiceTest.java
+++ src/test/java/edu/tamu/app/service/manager/GitHubProjectServiceTest.java
@@ -305,14 +305,24 @@ public class GitHubProjectServiceTest extends CacheMockTests {
         lenient().when(issueBuilder.body(any(String.class))).thenReturn(issueBuilder);
         lenient().when(issueBuilder.create()).thenReturn(testIssue1);
 
-        lenient().doReturn(String.valueOf(TEST_REPOSITORY1_ID)).when(testRepository1).getId();
+        lenient().when(testRepository1.getId()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return String.valueOf(TEST_REPOSITORY1_ID);
+            }
+        });
         lenient().when(testRepository1.getName()).thenReturn(TEST_REPOSITORY1_NAME);
         lenient().when(testRepository1.listProjects(any(ProjectStateFilter.class))).thenReturn(projectIterable);
         lenient().when(testRepository1.listProjects()).thenReturn(projectIterable);
         lenient().when(testRepository1.listLabels()).thenReturn(labelIterable);
         lenient().when(testRepository1.getIssues(any(GHIssueState.class))).thenReturn(testIssueList);
 
-        lenient().doReturn(String.valueOf(TEST_REPOSITORY2_ID)).when(testRepository2).getId();
+        lenient().when(testRepository2.getId()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return String.valueOf(TEST_REPOSITORY2_ID);
+            }
+        });
         lenient().when(testRepository2.getName()).thenReturn(TEST_REPOSITORY2_NAME);
         lenient().when(testRepository2.getIssues(any(GHIssueState.class))).thenReturn(testIssueList);
         lenient().when(testRepository2.listProjects()).thenReturn(projectIterable);
@@ -330,11 +340,36 @@ public class GitHubProjectServiceTest extends CacheMockTests {
         lenient().when(testColumn2.listCards()).thenReturn(cardIterable2);
         lenient().when(testColumn3.listCards()).thenReturn(cardIterable3);
 
-        lenient().doReturn(String.valueOf(1L)).when(testCard1).getId();
-        lenient().doReturn(String.valueOf(2L)).when(testCard2).getId();
-        lenient().doReturn(String.valueOf(3L)).when(testCard3).getId();
-        lenient().doReturn(String.valueOf(4L)).when(testCard4).getId();
-        lenient().doReturn(String.valueOf(5L)).when(testCard5).getId();
+        lenient().when(testCard1.getId()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return String.valueOf(1L);
+            }
+        });
+        lenient().when(testCard2.getId()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return String.valueOf(2L);
+            }
+        });
+        lenient().when(testCard3.getId()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return String.valueOf(3L);
+            }
+        });
+        lenient().when(testCard4.getId()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return String.valueOf(4L);
+            }
+        });
+        lenient().when(testCard5.getId()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return String.valueOf(5L);
+            }
+        });
 
         lenient().when(testCard1.getContent()).thenReturn(testIssue1);
         lenient().when(testCard2.getContent()).thenReturn(testIssue2);
@@ -366,7 +401,12 @@ public class GitHubProjectServiceTest extends CacheMockTests {
         lenient().when(testCard4.getContent().getAssignees()).thenReturn(testUsers4);
         lenient().when(testCard5.getContent().getAssignees()).thenReturn(testUsers5);
 
-        lenient().doReturn(String.valueOf(TEST_USER1_ID)).when(testUser1).getId();
+        lenient().when(testUser1.getId()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return String.valueOf(TEST_USER1_ID);
+            }
+        });
         lenient().when(testUser1.getName()).thenReturn(TEST_USER1_NAME);
         lenient().when(testUser1.getAvatarUrl()).thenReturn(TEST_USER1_AVATAR_PATH);
         lenient().when(testUser2.getAvatarUrl()).thenReturn(TEST_USER2_AVATAR_PATH);
